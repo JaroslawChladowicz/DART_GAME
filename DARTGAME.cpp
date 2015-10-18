9bf35072e9eac957abbe3c1e6cc2ca8ead68047b
Jarosław Chłądowicz <JaroslawChladowicz@users.noreply.github.com>
2015-10-18 10:59:48 +0200

    Create DARTGAME.cpp
    The main class. From this class start the program.

---
+++ DARTGAME.cpp
@@ -0,0 +1,18 @@
+// DARTGAME.cpp : Defines the entry point for the console application.
+//
+#include<iostream>
+#include<conio.h>
+#include"Game.h"
+#include"Player.h"
+using namespace std;
+
+
+int main()
+{
+	Game game;
+	game.game();
+	getchar();
+	getchar();
+	return 0;
+}
+
